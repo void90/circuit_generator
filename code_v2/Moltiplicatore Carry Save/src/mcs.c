bb2f665ffd9801e199dcdbeb5597846e5e5007c1
Diletta Lanini <diletta.lanini@gmail.com>
2020-07-12 11:42:32 -0700

    Moltiplicatore carry save

--- code_v2/Moltiplicatore Carry Save/src/mcs.c
+++ code_v2/Moltiplicatore Carry Save/src/mcs.c
@@ -6,9 +6,9 @@
 int main (int argc, char **argv)
 {
 //Inizializzazione variabili
-	int i, j, n, out_idx = 0;
-	char *param[3]={"out", "x", "y"};
-	char *param2[4] = {"sum", " Cout", "a", "b"};
+	short int i, j, out_idx=0;
+	char *param[3]={"out", "xin", "yin"};
+	char *param2[5] = {"sum", "Cout", "Cin", "a", "b"};
 	char fileInput[50]={"netlist/"};
 	strcat(fileInput, argv[1]);
 //Apertura file
@@ -19,56 +19,50 @@ int main (int argc, char **argv)
 		printf("File doesen't exist.\n");
 		return-1;
 	}
-	n = atoi(argv[2]);
+	int n = atoi(argv[2]);
 //Stampe netlist iniziali fisse
-	fprintf(fp, "*MOLTIPLICATORE CARRY SAVE\n\n.option filetype=ascii\n.INCLUDE ../lib/ST65LIKE_cell_library_v2020_1.net\n.INCLUDE ../lib/16nm_HP.pm\n.PARAM Lmin=16n\n.PARAM Wmin=16n\n.TRAN 0.1p 500p\n.PARAM XXX=1\n");
-//DICHIARAZIONE SOTTOCIRCUITO 4xAND2_SUB	
-	fprintf (fp, ".subckt AND_ARRAY_SUB\t0 Vdd ");
-	for( i = 0; i < 2; i++ ) 
+	fprintf(fp, "*MOLTIPLICATORE CARRY SAVE\n\n.option filetype=ascii\n.INCLUDE ../lib/ST65LIKE_cell_library_v2020_1.net\n.INCLUDE ../lib/16nm_HP.pm\n.PARAM Lmin=16n\n.PARAM Wmin=16n\n.TRAN 0.1p 500p\n.PARAM XXX=1\n\n* x & y sono ingressi\n.subckt AND_ARRAY_SUB 0 Vdd ");
+	//.subckt AND_ARRAY_SUB
+	for(i=0; i<2; i++)
 	{
-		for( j = 0; j < n; j++ )
+		for(j=0; j<n; j++)
 		{
 			fprintf(fp, "%s%d ", param[i], j);
 		}
 	}
-	fprintf(fp, "y\tXX=1\n");
-	for ( i = 0; i < n; i++)
+	fprintf(fp, "y XX = 1\n");
+	for(i=0; i<n; i++)
 	{
-		fprintf (fp,"xand%d\t0 Vdd out%d x%d y\tAND2_SUB XX=XXX\n", i, i, i);
+		fprintf(fp, "\txand%d 0 Vdd %s%d %s%d y AND2_SUB XX=XXX\n", i, param[0], i, param[1], i);
 	}
-	fprintf(fp, ".ends \n\n");
-
-//DICHIARAZIONE SOTTOCIRCUITO 3xFA_SUB	
-	fprintf (fp, ".subckt ADD_ARRAY_SUB\t0 Vdd ");
-	for( i = 0; i < 4; i++ ) 
+	//.subckt ADD_ARRAY_SUB
+	fprintf(fp, ".ends\n\n*ai &bi (i=0..2) sono in Adder\n.subckt ADD_ARRAY_SUB 0 Vdd ");
+	for(i=0; i<5; i++)
 	{
-		for( j = 0; j < n; j++)
+		for(j=0; j<n-1; j++)
 		{
 			fprintf(fp, "%s%d ", param2[i], j);
 		}
 	}
-	fprintf(fp, "\tXX=1\n");
-	for ( i = 0; i < n; i++)	//non è fino ad n!!!!!!
+	fprintf(fp, "XX = 1\n");
+	for(i=0; i<n-1; i++)
 	{
-		fprintf (fp,"xadd%d\t 0 vdd sum%d cout%d cin%d b%d a%d\t FA_SUB XX=XXX\n", i, i, i, i, i, i);
+		fprintf(fp, "\txadd%d 0 Vdd sum%d cout%d cin%d a%d b%d FA_SUB XX = XXX\n", i, i, i, i, i, i);
 	}
-	fprintf(fp, ".ends \n\n");
-	
-//DICHIARAZIONE MCS_SUB
-	fprintf (fp, ".subckt MCS_SUB\t0 Vdd ");
+	//.subckt MCS_SUB
+	fprintf(fp, ".ends\n\n* Zi->Uscute moltiplicatore; c->Carry d'uscita; Mj,Nj->Fattori d'ingresso. 	-----	Con [i=0,..,7; j=0,..,3]\n.subckt MCS_SUB 0 Vdd ");
 	for(i=0; i<n*2; i++)
 	{
 		fprintf(fp, "z%d ", i);
 	}
-	for( i = 0; i < 3; i++ ) 
+	for(i=1; i<3; i++)
 	{
-		for( j = 0; j < n; j++)
+		for(j=0; j<n; j++)
 		{
 			fprintf(fp, "%s%d ", param[i], j);
 		}
 	}
-	fprintf(fp, "\tXX=1\n");
-	
+	fprintf(fp, "XX = 1\n");
 	int num_level = n*2;
 	//Gestire: se n=1 deve essere solo una porta AND
 
@@ -210,21 +204,81 @@ int main (int argc, char **argv)
 		}
 	}
 	fprintf(fp, ".ends \n\n");
-//Dichiarazione del componente 
-	fprintf(fp, "xmcs%d 0 Vcc ",n );
-	for ( i = 0; i<num_level; i++)
+
+/*	for(i=0; i<=n; i++)
 	{
-		fprintf(fp, "xz%d ",i);
+		if(i!=n)
+		{
+			fprintf(fp, "\txand4_level%d 0 Vdd ", i);
+			for(j=0; j<n; j++)
+			{
+				if(i==0 && j==0)
+				{	fprintf(fp, "z0 "); j=1;}
+				fprintf(fp, "%dout%d ", i, j);
+			}
+			for(j=0; j<n; j++)
+			{
+				fprintf(fp, "xin%d ", j);
+			}
+			fprintf(fp, "yin%d AND_ARRAY_SUB XX = XXX\n", i);
+		}
+		if(i!=0)
+		{
+			fprintf(fp, "\txadd_array%d 0 Vdd z%d ", i-1, i);
+			if(i==n)
+			{
+				for(j=n+1; j<n*2-1; j++)
+				{
+					fprintf(fp, "z%d ", j);
+				}
+			}
+			else
+			{
+				for(j=1; j<n-1; j++)
+				{
+					fprintf(fp, "%dsum%d ", i-1, j);
+				}
+			}
+			for(z=i-1; z>=0; z--)
+			{
+				for(j=0; j<n-1; j++)
+				{
+					if(i==n && j==n-2 && z==i-1)
+					{
+						fprintf(fp, "z%d 0 ", n*2-1);
+					}
+					else
+					{
+						if(i>=0)
+						{
+							fprintf(fp, "%dcout%d ", i-1, j);
+						}
+						else
+						{
+							fprintf(fp, "0 ");
+						}
+					}
+				}
+			}
+fprintf(fp, "%dout%d ADD_ARRAY_SUB XX = XXX\n", i-1, n-1);
+		}
 	}
-	for ( i = 0; i<n; i++)
+	fprintf(fp, ".ends\n\n");
+*/
+	//DICHIARAZIONE COMPONENTE
+	fprintf(fp, "\n\nxmcs 0 Vcc ");
+	for(i=0; i<n*2; i++)
 	{
-		fprintf(fp, "xx%d ",i);
+		fprintf(fp, "zz%d ", i);
 	}
-	for ( i = 0; i<n; i++)
+	for(i=1; i<3; i++)
 	{
-		fprintf(fp, "xy%d ",i);
+		for(j=0; j<n; j++)
+		{
+			fprintf(fp, "z%c%d ", param[i][0], j);
+		}
 	}
-	fprintf(fp, "MCS_SUB XX = XXX\n");
+	fprintf(fp, " MCS_SUB XX = XXX\n");
 //Fine codice generazione netlist
 //Conversione operandi di ingresso
 	int x, y;
